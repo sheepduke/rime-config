e4825fbe13cbd8d425d5cf094785edea3939ff94
YUE Daian <sheepduke@gmail.com>
2026-06-04 23:48:20 +0800

    加入虎码

---
+++ rime.lua
@@ -0,0 +1,7 @@
+charset_comment_filter = require("charset_comment_filter") --Unicode分区提示
+core2022 = require("core2022_filter") --自定义字符集过滤（常用字集）
+number_translator = require("number")
+lua_unicode_display_filter = require("unicode_display")  --Unicode编码提示
+calculator_translator = require("calculator_translator")  --简易计算器
+exe_processor = require("exe")  -- 网页启动器
+shijian2_translator = require("shijian2") -- 高级时间
