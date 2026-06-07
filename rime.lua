5943e608663ac207ac968694e0641fa25b7914d7
YUE Daian <sheepduke@gmail.com>
2026-06-07 09:32:25 +0800

    Make it work

--- rime.lua
+++ rime.lua
@@ -1,7 +1,8 @@
-charset_comment_filter = require("tiger_charset_comment_filter") --Unicode分区提示
-tiger_core2022 = require("tiger_core2022_filter") --自定义字符集过滤（常用字集）
-number_translator = require("tiger_number")
-lua_unicode_display_filter = require("tiger_unicode_display")  --Unicode编码提示
-calculator_translator = require("tiger_calculator_translator")  --简易计算器
-exe_processor = require("tiger_exe")  -- 网页启动器
-shijian2_translator = require("tiger_shijian2") -- 高级时间
+charset_comment_filter = require("charset_comment_filter") --Unicode分区提示
+core2022 = require("core2022_filter") --自定义字符集过滤（常用字集）
+-- dz_ci = require("dz_ci_filter") --单字模式 这个别用，有问题的
+number_translator = require("number")
+lua_unicode_display_filter = require("unicode_display")  --Unicode编码提示
+calculator_translator = require("calculator_translator")  --简易计算器
+exe_processor = require("exe")  -- 网页启动器
+shijian2_translator = require("shijian2") -- 高级时间
